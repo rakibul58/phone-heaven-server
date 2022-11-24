b3a7c890bf74e77486b50a1437027a98b760ff58
rakibul58 <rhrahi14@gmail.com>
2022-11-24 11:16:57 +0600

    categories api added

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
 
 const port = process.env.PORT || 5000;
 
@@ -8,6 +10,33 @@ const app = express();
 // middlewares
 app.use(cors());
 app.use(express.json());
+
+// mongodb
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bebjeyw.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run() {
+
+    try {
+        // collections
+        const categoriesCollection = client.db("mobileHeaven").collection("categories");
+
+        app.get('/categories', async (req, res) => {
+            const query = {};
+            const result = await categoriesCollection.find(query).toArray();
+            res.send(result);
+        });
+
+    }
+    finally {
+
+    }
+
+}
+
+run().catch(err => console.error(err));
+
 
 app.get('/', async (req, res) => {
     res.send('Mobile Heaven server is running')
