a33d229f0cdabd61551a6b00dc3d7c25d4b04bbd
rakibul58 <rhrahi14@gmail.com>
2022-11-25 17:40:25 +0600

    is admin added

--- index.js
+++ index.js
@@ -68,11 +68,20 @@ async function run() {
             res.send(result);
         });
 
+        // for selllers 
         app.get('/users/seller/:email', async (req, res) => {
             const email = req.params.email;
             const query = { email };
             const user = await usersCollection.findOne(query);
             res.send({ isSeller: user?.role === 'seller' });
+        });
+
+        // for admin 
+        app.get('/users/admin/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { email };
+            const user = await usersCollection.findOne(query);
+            res.send({ isAdmin: user?.role === 'admin' });
         });
 
     }
