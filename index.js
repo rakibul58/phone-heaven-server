9a8da37ff680299231a7b0e795511c439a827791
rakibul58 <rhrahi14@gmail.com>
2022-11-25 15:08:01 +0600

    is seller api added

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
+const jwt = require('jsonwebtoken');
 
 const port = process.env.PORT || 5000;
 
@@ -15,6 +16,21 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bebjeyw.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'unauthorized access' });
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden access' });
+        }
+        req.decoded = decoded;
+        next();
+    });
+}
+
 
 async function run() {
 
@@ -22,6 +38,14 @@ async function run() {
         // collections
         const categoriesCollection = client.db("mobileHeaven").collection("categories");
         const usersCollection = client.db("mobileHeaven").collection("users");
+
+        app.get('/jwt', async (req, res) => {
+            const email = req.query.email;
+
+            const token = jwt.sign({ email }, process.env.ACCESS_TOKEN, { expiresIn: '7h' });
+            return res.send({ accessToken: token });
+
+        });
 
         // fetch categories 
         app.get('/categories', async (req, res) => {
@@ -31,17 +55,24 @@ async function run() {
         });
 
         // add users 
-        app.post('/users' , async(req, res)=>{
+        app.post('/users', async (req, res) => {
             const user = req.body;
             // console.log(user);
-            const query = {email: user.email}
+            const query = { email: user.email }
             const userExists = await usersCollection.findOne(query);
-            
-            if(userExists){
+
+            if (userExists) {
                 return;
             }
             const result = await usersCollection.insertOne(user);
             res.send(result);
+        });
+
+        app.get('/users/seller/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { email };
+            const user = await usersCollection.findOne(query);
+            res.send({ isSeller: user?.role === 'seller' });
         });
 
     }
